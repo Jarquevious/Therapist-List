163c5ab8a7f2fd75a180f1451740ba4a5660cfe3
Jarquevious <jarquevious@gmail.com>
2019-10-18 14:25:40 -0700

    fuctional web

--- app.py
+++ app.py
@@ -6,13 +6,11 @@ from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Therapy')
-client = MongoClient(host=host)
-db = client.Therapy
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
 therapists = db.therapists
 
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def home():
@@ -22,13 +20,12 @@ def home():
 @app.route('/therapists')
 def index():
     """Show all therapists."""
-    return render_template('index.html', therapists=therapists.find())
+    return render_template('therapist_index.html', therapists=therapists.find())
 
 @app.route('/therapists/add')
 def therapists_add():
     """Add a new therapist."""
-    return render_template('therapists_add.html')
-
+    return render_template('therapists_add.html', therapist={}, title='New Therapist')
 
 @app.route('/therapists', methods=['POST'])
 def therapist_submit():
@@ -50,15 +47,52 @@ def therapist_submit():
         'phone': request.form.get('phone'),
         'approach': request.form.get('approach'),
         'issues': request.form.get('issues')
-        
-        
-        
-         }
-    therapists.insert_one(therapist)
-    return redirect(url_for('index'))
+    }
+    therapist_id = therapists.insert_one(therapist).inserted_id
+    return redirect(url_for('therapist_show',therapist_id = therapist_id))
+         
+@app.route('/therapists/<therapist_id>')
+def therapist_show(therapist_id):
+    """Show a single therapist."""
+    therapist = therapists.find_one({'_id': ObjectId(therapist_id)})
+    return render_template('therapist_show.html', therapist=therapist)
 
+@app.route('/therapists/<therapist_id>/edit')
+def therapists_edit(therapist_id):
+    """Show the edit form for a therapist."""
+    therapist = therapists.find_one({'_id': ObjectId(therapist_id)})
+    return render_template('therapists_edit.html', therapist=therapist, title='Edit Therapist')
 
+@app.route('/therapists/<therapist_id>', methods=['POST'])
+def therapists_update(therapist_id):
+    """Submit an edited therapist."""
+    updated_therapist = {
+        'name': request.form.get('name'),
+        'phone': request.form.get('phone'),
+        'email': request.form.get('email'),
+        'street': request.form.get('street'),
+        'city': request.form.get('city'),
+        'state': request.form.get('state'),
+        'zip': request.form.get('zip'),
+        'status': request.form.get('status'),
+        'billing': request.form.get('billing'),
+        'services': request.form.get('services'),
+        'ages': request.form.get('ages'),
+        'language': request.form.get('language'),
+        'profession': request.form.get('profession'),
+        'approach': request.form.get('approach'),
+        'issues': request.form.get('issues'),
+    }
+    therapists.update_one(
+        {'_id': ObjectId(therapist_id)},
+        {'$set': updated_therapist})
+    return redirect(url_for('index', therapist_id=therapist_id))
 
+@app.route('/therapists/<therapist_id>/delete', methods=['POST'])
+def therapists_delete(therapist_id):
+    """Delete one therapist."""
+    therapists.delete_one({'_id': ObjectId(therapist_id)})
+    return render_template('therapist_index.html', therapists=therapists.find())
 
 
 
